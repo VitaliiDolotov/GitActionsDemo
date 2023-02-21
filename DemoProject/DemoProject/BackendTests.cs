8acb7a8f05df29b82cc34cd886efd2879b8cbe0f
Vitalii Dolotov <vitaliy.dolotov@gmail.com>
2023-02-21 22:34:43 +0200

    Added .Net solution with unit and backend tests

---
+++ DemoProject/DemoProject/BackendTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RestSharp;
+using System.Net;
+
+namespace BackTests
+{
+    internal class BackendTests
+    {
+        [Test]
+        [CategoryAttribute("OnBuild")]
+        public void SomeUnitTest()
+        {
+            var client = new RestClient("https://gorest.co.in/public/v2/");
+            var request = new RestRequest("posts");
+            var response = client.Get(request);
+
+            response
+                .StatusCode
+                .Should()
+                .Be(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ DemoProject/Tests/UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    internal class UnitTests
+    {
+        [Test]
+        [CategoryAttribute("OnBuild")]
+        public void SomeUnitTest()
+        {
+            var expectedValue = "VALUE";
+            var actualValue = "VALUE";
+
+            actualValue
+                .Should()
+                .BeEquivalentTo(expectedValue);
+        }
+    }
+}
